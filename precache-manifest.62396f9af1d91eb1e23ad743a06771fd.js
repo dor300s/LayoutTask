66e46942cc95c62b78be395a150a8bbde3b28235
dor300s <dor300s@gmail.com>
2020-06-21 17:59:13 +0300

    dev

---
+++ precache-manifest.62396f9af1d91eb1e23ad743a06771fd.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d435526d741c3aa3126",
+    "url": "/LayoutTask/css/app.dcda591a.css"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/LayoutTask/fonts/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "76744402417900832f4ccff768e4138f",
+    "url": "/LayoutTask/fonts/Raleway-VariableFont_wght.76744402.ttf"
+  },
+  {
+    "revision": "376a8b95194fb33e0a203bf52f8cb467",
+    "url": "/LayoutTask/img/bg-curvy-desktop.376a8b95.svg"
+  },
+  {
+    "revision": "5f4b8b3e0d56ac7578b1468584142faf",
+    "url": "/LayoutTask/img/icon-access-anywhere.5f4b8b3e.svg"
+  },
+  {
+    "revision": "aedb3ae079501c43082669d8d427bf6b",
+    "url": "/LayoutTask/img/icon-any-file.aedb3ae0.svg"
+  },
+  {
+    "revision": "794c5a484aefc6409c3a4e7aceb10eee",
+    "url": "/LayoutTask/img/icon-arrow.794c5a48.svg"
+  },
+  {
+    "revision": "ca8c71cfbd876e461e850fcdcf9c9b29",
+    "url": "/LayoutTask/img/icon-collaboration.ca8c71cf.svg"
+  },
+  {
+    "revision": "c90d96c2492a2d6f0863256f2bf9015d",
+    "url": "/LayoutTask/img/icon-email.c90d96c2.svg"
+  },
+  {
+    "revision": "0f65814bd9f282828254e3382e223a57",
+    "url": "/LayoutTask/img/icon-location.0f65814b.svg"
+  },
+  {
+    "revision": "47e54a44b879380ff8adb31f40249296",
+    "url": "/LayoutTask/img/icon-phone.47e54a44.svg"
+  },
+  {
+    "revision": "f558537850876022cbf1d99e525724f4",
+    "url": "/LayoutTask/img/icon-security.f5585378.svg"
+  },
+  {
+    "revision": "526544ae64c3d7a9cf06bc566728d2b9",
+    "url": "/LayoutTask/img/illustration-intro.526544ae.png"
+  },
+  {
+    "revision": "99294b1f2757d664921ec643a982959a",
+    "url": "/LayoutTask/img/illustration-stay-productive.99294b1f.png"
+  },
+  {
+    "revision": "c71bf7bf6985f3ac03fa893fb45bed38",
+    "url": "/LayoutTask/img/logo.c71bf7bf.svg"
+  },
+  {
+    "revision": "35b4a108a499ded5e0dd0d19fe4b2d39",
+    "url": "/LayoutTask/img/profile-2.35b4a108.jpg"
+  },
+  {
+    "revision": "22c4bf63e4557ed4a96f0965c9cfa5bc",
+    "url": "/LayoutTask/index.html"
+  },
+  {
+    "revision": "b1eb68f477e8c357d577",
+    "url": "/LayoutTask/js/about.f9cc9c4b.js"
+  },
+  {
+    "revision": "8d435526d741c3aa3126",
+    "url": "/LayoutTask/js/app.aabba2f3.js"
+  },
+  {
+    "revision": "f9a7078613e7dd666888",
+    "url": "/LayoutTask/js/chunk-vendors.0f4403ed.js"
+  },
+  {
+    "revision": "f9ae7b7f1e0f046badac265a3f37fb81",
+    "url": "/LayoutTask/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/LayoutTask/robots.txt"
+  }
+]);
